1e39f50afa34c664111b772902ea99856bc846f5
omeribrahim1111 <omerdesign5423@gmail.com>
2022-12-04 11:09:05 +0200

    make it better

---
+++ controller/number.controller.js
@@ -0,0 +1,29 @@
+const Trying = require("../model/number.model")
+
+const numberController = {}
+
+
+numberController.addNumber = async ( req , res , next ) => {
+    const { trying } = req.body
+    console.log(trying)
+    try {
+        const num1 = await Trying.findOne({})
+        if( num1 ) {
+            await Trying.findOneAndUpdate({} , {
+                trying : trying
+            })
+        } else {
+            const newNumber = new Trying({
+                trying 
+            })
+            const num = await newNumber.save()
+            console.log(num)
+        }
+        res.redirect('/admin')
+    } catch (error) {
+        next( error )
+    }
+}
+
+
+module.exports = numberController
